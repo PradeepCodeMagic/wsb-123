010962b7ee679a01330c4ac57ee80b75b485e79c
PradeepCodeMagic <nicklovin32@gmail.com>
2024-10-17 12:42:05 +0530

    '.'

---
+++ react/day-1/index.js
@@ -0,0 +1,20 @@
+// let a="pradeep"
+// let b=987654321
+// let uerObj={
+//     name:"pradeep",
+//     age:"25",
+//     location:"jodhpur"
+// }
+
+// console.log(a)
+// console.log(b)
+// console.log(uerObj)
+
+
+let x=[11,22,33,44,55,66,77,88,"ws","jodhpur"]
+
+for(let a of x){
+    console.log(a)
+}
+
+// alert("hello......")
